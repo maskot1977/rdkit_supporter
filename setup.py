6c978fb7b0693a87e6a81c78a424f149d565eaf9
Ikemen Mas Kot <maskot1977@gmail.com>
2020-11-05 22:33:33 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="rdkit_installer",
+    version="0.1.0",
+    author="masaaki-kotera",
+    author_email="maskot1977@gmail.com",
+    description="rdkit_installer",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/maskot1977/rdkit_installer/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points = {
+        'console_scripts': ['sample_command = sample_command.sample_command:main']
+    },
+    python_requires='>=3.6',
+)
