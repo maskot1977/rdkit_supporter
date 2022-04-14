e95dbbd3502ff271ca612957f035bf44bbaf487b
Ikemen Mas Kot <maskot1977@gmail.com>
2022-04-14 17:27:17 +0900

    Update preprocess.py

--- rdkit_installer/preprocess.py
+++ rdkit_installer/preprocess.py
@@ -2,6 +2,33 @@ import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 
 
+def remove_low_variance_features(df, threshold=0.0):
+    ok_id = []
+    for colid, col in enumerate(df.values.T):
+        try:
+            if np.var(col) > threshold:
+                ok_id.append(colid)
+        except:
+            pass
+
+    return df.iloc[:, ok_id]
+
+
+def remove_high_correlation_features(df, threshold=0.95):
+    corrcoef = np.corrcoef(df.T.values.tolist())
+    selected_or_not = {}
+    for i, array in enumerate(corrcoef):
+        if i not in selected_or_not.keys():
+            selected_or_not[i] = True
+        if selected_or_not[i]:
+            for j, ary in enumerate(array):
+                if i < j:
+                    if abs(ary) >= threshold:
+                        selected_or_not[j] = False
+
+    return df.iloc[:, [i for i, array in enumerate(corrcoef) if selected_or_not[i]]]
+
+
 class TableCleaner:
     def __init__(self):
         self.model = RandomForestRegressor(n_estimators=1, max_depth=1, n_jobs=-1)
