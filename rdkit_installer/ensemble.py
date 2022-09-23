cf76657bc1c844a6d097e17c4dafca9cf44f6e33
Ikemen Mas Kot <maskot1977@gmail.com>
2022-09-24 07:41:33 +0900

    Update ensemble.py

--- rdkit_installer/ensemble.py
+++ rdkit_installer/ensemble.py
@@ -160,10 +160,10 @@ class SmilesBaggingMLP:
                 else:
                     print(data_df[self.target_col])
                     print(self.Y_df.iloc[:, selected_col_copy])
-                    print(self.Y_df.iloc[:, selected_col_copy].mode(axis=1).dropna(axis=0))
+                    print(self.Y_df.iloc[:, selected_col_copy].dropna(axis=0).mode(axis=1))
                     score = balanced_accuracy_score(
                                     data_df[self.target_col],
-                                    self.Y_df.iloc[:, selected_col_copy].mode(axis=1).dropna(axis=0))                    
+                                    self.Y_df.iloc[:, selected_col_copy].dropna(axis=0).mode(axis=1))                    
                 
                 if best_score < score:
                     best_score = score
