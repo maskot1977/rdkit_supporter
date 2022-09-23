dddb041c40f3d4edf0bb089f926502112abc3ddc
Ikemen Mas Kot <maskot1977@gmail.com>
2022-09-24 06:50:21 +0900

    Update ensemble.py

--- rdkit_installer/ensemble.py
+++ rdkit_installer/ensemble.py
@@ -158,6 +158,8 @@ class SmilesBaggingMLP:
                                     data_df[self.target_col],
                                     self.Y_df.iloc[:, selected_col_copy].mean(axis=1))
                 else:
+                    print(data_df[self.target_col])
+                    print(self.Y_df.iloc[:, selected_col_copy].mode(axis=1))
                     score = balanced_accuracy_score(
                                     data_df[self.target_col],
                                     self.Y_df.iloc[:, selected_col_copy].mode(axis=1))                    
